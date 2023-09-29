531f226e0bdc77ac5a3b0d86b558d9b3053fad3b
cegb1402 <146489656+cegb1402@users.noreply.github.com>
2023-09-29 17:27:29 -0600

    Activity Prinicipal

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText edCliente;
+    private Button btnIngresar,btnSalir;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edCliente=findViewById(R.id.edEmpleado);
+        btnIngresar=findViewById(R.id.btnIngresar);
+        btnSalir=findViewById(R.id.btnSalir);
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Cliente=edCliente.getText().toString().trim();
+                if (edCliente.getText().toString().matches("")){
+                    Toast.makeText(MainActivity.this, "No ingreso cliente", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Intent intento=new Intent(MainActivity.this, MainActivity2.class);
+                    intento.putExtra("Cliente",Cliente);
+                    startActivity(intento);
+                    finish();
+                }
+
+            }
+        });
+
     }
 }
